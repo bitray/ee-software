b0379c1b4863bad0770981821b3e902c21e0e7a6
tebi <bitray19850330@gmail.com>
2017-05-22 22:13:42 +0800

    为了防止netbeans异常

--- src/main/java/net/ray/web/ees/bo/impl/BrandInfoBOImpl.java
+++ src/main/java/net/ray/web/ees/bo/impl/BrandInfoBOImpl.java
@@ -8,14 +8,14 @@ import org.springframework.stereotype.Service;
 
 import net.ray.web.ees.bo.BrandInfoBO;
 import net.ray.web.ees.db.dao.BrandInfroMapper;
-import net.ray.web.ees.db.eo.BrandInfro;
+import net.ray.web.ees.db.eo.BrandInfo;
 @Service
 public class BrandInfoBOImpl implements BrandInfoBO {
 	@Resource
 	private BrandInfroMapper brandInfroMapper;
 	
 	@Override
-	public List<BrandInfro> getBrandsBySelective(BrandInfro brandInfo) {
+	public List<BrandInfo> getBrandsBySelective(BrandInfo brandInfo) {
 		return brandInfroMapper.selectBySelective(brandInfo);
 	}
 
