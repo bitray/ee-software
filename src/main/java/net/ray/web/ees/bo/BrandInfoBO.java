fb198b655f69519db052b3d927f1c9679c21e7b5
tebi <bitray19850330@gmail.com>
2017-05-23 09:38:11 +0800

    调整

--- src/main/java/net/ray/web/ees/bo/BrandInfoBO.java
+++ src/main/java/net/ray/web/ees/bo/BrandInfoBO.java
@@ -3,7 +3,7 @@ package net.ray.web.ees.bo;
 import java.util.List;
 
 import net.ray.web.ees.db.eo.BrandInfo;
-
+ 
 public interface BrandInfoBO {
 	public List<BrandInfo> getBrandsBySelective(BrandInfo brandInfo);
 }
